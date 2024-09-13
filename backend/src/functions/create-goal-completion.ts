b402a95b4aaf3649a409d6c51c1a40d2f3201d5e
Diego <diegoramos3709@gmail.com>
2024-09-13 09:58:07 -0300

    Criando função que define metas concluídas

---
+++ backend/src/functions/create-goal-completion.ts
@@ -0,0 +1,55 @@
+import { and, count, eq, gte, lte, sql } from 'drizzle-orm'
+import { db } from '../db'
+import { goalCompletions, goals } from '../db/schema'
+import dayjs from 'dayjs'
+
+interface CreateGoalCompletionRequest {
+  goalId: string
+}
+
+export async function CreateGoalCompletion({
+  goalId,
+}: CreateGoalCompletionRequest) {
+  const lastDayOfWeek = dayjs().endOf('week').toDate()
+  const firstDayOfWeek = dayjs().startOf('week').toDate()
+
+  const goalCompletionCounts = db.$with('goal_completion_counts').as(
+    db
+      .select({
+        goalId: goalCompletions.goalId,
+        completionCount: count(goalCompletions.id).as('completionCount'),
+      })
+      .from(goalCompletions)
+      .where(
+        and(
+          gte(goalCompletions.createdAt, firstDayOfWeek),
+          lte(goalCompletions.createdAt, lastDayOfWeek)
+        )
+      )
+      .groupBy(goalCompletions.goalId)
+  )
+
+  const result = await db
+    .with(goalCompletionCounts)
+    .select({
+      desiredWeeklyFrequency: goals.desiredWeeklyFrequency,
+      completionCount: sql`
+         COALESCE(${goalCompletionCounts.completionCount}, 0)
+        `.mapWith(Number),
+    })
+    .from(goals)
+    .leftJoin(goalCompletionCounts, eq(goalCompletionCounts.goalId, goals.id))
+
+  const result = await db
+    .insert(goalCompletions)
+    .values({
+      goalId,
+    })
+    .returning()
+
+  const goalCompletion = result[0]
+
+  return {
+    goalCompletion,
+  }
+}
