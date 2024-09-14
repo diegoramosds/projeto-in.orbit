be92e3f2a09ab0b3227ec69c7e8ed44a79496a76
Diego <diegoramos3709@gmail.com>
2024-09-14 17:34:31 -0300

    Usando tanstack

--- frontend/web/src/App.tsx
+++ frontend/web/src/App.tsx
@@ -1,13 +1,21 @@
 import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/create-goals'
 import { Summary } from './components/summary'
- 
+import { useEffect, useState } from 'react'
+import { EmptyGoals } from './components/empty-goals'
+import { useQuery } from '@tanstack/react-query'
+import { getSummary } from './http/get-summary'
+
 
 export function App() {
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+  })
+
   return (
     <Dialog>
-      {/* <EmptyGoals /> */}
-      <Summary />
+      {data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
       <CreateGoal />
     </Dialog>
   )
