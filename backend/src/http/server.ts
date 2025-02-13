5ef2b2fa94384982eaa1543550d2338b5b933364
Diego <diegoramos3709@gmail.com>
2025-02-13 17:47:38 -0300

    Corrigindo binding de host para Heroku

--- backend/src/http/server.ts
+++ backend/src/http/server.ts
@@ -28,6 +28,7 @@ app.register(getWeekSummaryRoute)
 app
   .listen({
     port: Number(process.env.PORT) || 3333,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log('HTTP server running!')
