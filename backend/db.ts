94799aada7a6b9adbc69d4db5aa5d3484487322e
Diego <diegoramos3709@gmail.com>
2025-02-20 20:17:02 -0300

    Atualiza a configuração do banco de dados

--- backend/db.ts
+++ backend/db.ts
@@ -7,7 +7,7 @@ config();
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false,
+    rejectUnauthorized: false, // Isso pode ser usado para desativar a validação do certificado
   },
 });
 
